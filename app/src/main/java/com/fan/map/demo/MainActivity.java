af87620ada23f9b3e0c4d94eb8419524d847a318
qifan <13076774@cnsuning.com>
2017-02-08 21:38:35 +0800

    sdk 集成

--- app/src/main/java/com/fan/map/demo/MainActivity.java
+++ app/src/main/java/com/fan/map/demo/MainActivity.java
@@ -8,14 +8,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.fan.map.demo.amap.CustomLocation2DActivity;
+import com.fan.map.demo.amap.CustomLocationActivity;
 import com.fan.map.demo.baidu.LocationMark;
 import com.fan.map.demo.gps.GPSService;
+import com.fan.map.demo.tecent.ShowMyLocationActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button btnGps, btnBaidu, btnGaode, btnTecent;
+    private Button btnGps, btnBaidu, btnGaode, btnTecent,btnAmap3D;
     private TextView tvGpsResult;
-
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,31 +29,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnBaidu = (Button) findViewById(R.id.baidu);
         btnTecent = (Button) findViewById(R.id.tecent);
         btnGaode = (Button) findViewById(R.id.gaode);
-
+        btnAmap3D = (Button) findViewById(R.id.gaode3d);
         btnGps.setOnClickListener(this);
         btnBaidu.setOnClickListener(this);
         btnGaode.setOnClickListener(this);
         btnTecent.setOnClickListener(this);
-
+        btnAmap3D.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+
             case R.id.gps:
                 Log.d("qifan","click" );
 
-                Intent gpsIntent = new Intent(MainActivity.this, GPSService.class);
-                startService(gpsIntent);
+                intent = new Intent(MainActivity.this, GPSService.class);
+                startService(intent);
                 break;
             case R.id.baidu:
-                Intent intent = new Intent(MainActivity.this, LocationMark.class);
+                intent = new Intent(MainActivity.this, LocationMark.class);
                 startActivity(intent);
                 break;
             case R.id.tecent:
+                intent = new Intent(MainActivity.this, ShowMyLocationActivity.class);
+                startActivity(intent);
                 break;
             case R.id.gaode:
+                intent = new Intent(MainActivity.this, CustomLocation2DActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.gaode3d:
+                intent = new Intent(MainActivity.this, CustomLocationActivity.class);
+                startActivity(intent);
                 break;
             default:
                 break;
